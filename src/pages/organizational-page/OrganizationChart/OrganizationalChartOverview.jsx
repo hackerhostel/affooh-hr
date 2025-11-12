505e9d5346fb81eea7935dd406409e5d2125275c
NipunS12 <114478939+NipunS12@users.noreply.github.com>
2025-11-12 01:17:49 -0800

    update
    update

--- src/pages/organizational-page/OrganizationChart/OrganizationalChartOverview.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const OrganizationalChartOverview = () => {
-  return (
-    <div>OrganizationalChartOverview</div>
-  )
-}
-
-export default OrganizationalChartOverview
